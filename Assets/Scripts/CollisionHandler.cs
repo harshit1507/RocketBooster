972ead049fdf49ad1e268ff6bab885ae95b48847
harshit1507 <harshit1507@yahoo.com>
2021-03-09 23:44:05 +0530

    Reload level on crash

---
+++ Assets/Scripts/CollisionHandler.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CollisionHandler : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        switch(collision.gameObject.tag)
+        {
+            case "Friendly":
+                Debug.Log("Bumped Into Launch Pad");
+                break;
+            case "Finish":
+                Debug.Log("Bumped into Finish Pad");
+                break;
+            case "Fuel":
+                Debug.Log("You picked up fuel");
+                break;
+            default:
+                ReloadLevel();
+                break;
+        }
+    }
+
+    private void ReloadLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
+    }
+}
